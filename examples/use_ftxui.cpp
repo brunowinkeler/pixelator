5fdd8732d3709e0eaf1886cfc33ac90d719e9d66
Bruno Winkeler <brunowinkeler@gmail.com>
2024-02-10 18:39:59 +0100

    Add gitignore and check size of terminal with ftxui example

--- examples/use_ftxui.cpp
+++ examples/use_ftxui.cpp
@@ -1,19 +1,28 @@
 #include "ftxui/screen/color.hpp"
 #include "ftxui/screen/screen.hpp"
 
+#include <iostream>
+
 namespace {
 const ftxui::Color kYellowishColor = ftxui::Color::RGB(255, 200, 100);
-}
+const ftxui::Color kBlueishColor = ftxui::Color::RGB(0, 100, 200);
+}  // namespace
 
 int main() {
   const ftxui::Dimensions dimensions{ftxui::Dimension::Full()};
   ftxui::Screen screen{ftxui::Screen::Create(dimensions)};
-  auto &pixel_left = screen.PixelAt(10, 10);
+  auto &pixel_left = screen.PixelAt(0, 0);
   pixel_left.background_color = kYellowishColor;
-  pixel_left.character = ' ';
-  auto &pixel_right = screen.PixelAt(11, 10);
-  pixel_right.background_color = kYellowishColor;
-  pixel_right.character = ' ';
+  pixel_left.character = 'X';
+  auto &pixel_right = screen.PixelAt(ftxui::Terminal::Size().dimx - 1,
+                                     ftxui::Terminal::Size().dimy - 1);
+  pixel_right.background_color = kBlueishColor;
+  pixel_right.character = 'O';
   screen.Print();
+
+  std::cout << "Dimensions:" << std::endl;
+  std::cout << "X: " << ftxui::Terminal::Size().dimx << std::endl;
+  std::cout << "Y: " << ftxui::Terminal::Size().dimy << std::endl;
+
   return 0;
 }
